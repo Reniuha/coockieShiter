a263e46ca4339cf080f77ddc004c501d0b250471
Reniuha <naglisr45@gmail.com>
2024-12-19 21:55:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ const coockie = document.getElementById("coockie");
 var counterElement = document.getElementById("coockie-counter");
 var rebirthButton = document.getElementById("rebirth");
 
-var counter = 0;
+var counter = 5000;
 var rebirth = 1;
-
 var price = 20;
-
-
 
 const clicker = document.getElementById("clicker");
 let clickerOwnedElement = document.getElementById("owned");
@@ -17,13 +14,9 @@ let clickerOwned = 0;
 let clickerPrice = 20;
 
 coockie.addEventListener("click", () => {
-    
     counter += rebirth === 0 ? 1 : 1 * rebirth;
-
     console.log("counter: " + counter);
-    counterElement.textContent = `Cookies: ${counter} / Rebirths: ${rebirth - 1} /  Rebirth price: ${price}`;
-
-    enoughForClicker()
+    enoughForClicker();
 });
 
 clicker.addEventListener("click", () => {
@@ -31,10 +24,7 @@ clicker.addEventListener("click", () => {
         counter -= clickerPrice;
         clickerOwned += 1;
         clickerPrice = Math.round(clickerPrice * 1.5);
-
-        clickerOwnedElement.textContent = `Owned: ${clickerOwned}`;
-        clickerPriceElement.textContent = `Price: ${clickerPrice}`;
-
+        enoughForClicker();
     } else {
         const previousText = clickerPriceElement.textContent;
         clickerPriceElement.textContent = "You don't have enough cookies.";
@@ -42,41 +32,40 @@ clicker.addEventListener("click", () => {
             clickerPriceElement.textContent = previousText;
         }, 2000);
     }
-
-
-
-
-    enoughForClicker()
 });
-
-
 
 rebirthButton.addEventListener("click", () => {
     if (counter >= price) {
         counter = 0;
         rebirth += 1;
-        console.log("rebirth: " + rebirth);
         price = Math.round(price * 2.5);
-        
         clickerOwned = 0;
         clickerPrice = 20;
-
-        clickerPriceElement.textContent = `Price: ${clickerPrice}`;
-        clickerOwnedElement.textContent = `Owned: ${clickerOwned}`;
-        counterElement.textContent = `Cookies: ${counter} / Rebirths: ${rebirth - 1} / Rebirth price: ${price}`;
+        console.log("rebirth: " + rebirth);
     } else {
         counterElement.textContent = `You don't have enough cookies.`;
     }
-
-    enoughForClicker()
+    enoughForClicker();
 });
 
-function enoughForClicker () {
-    if(counter >= clickerPrice){
-        clicker.style.backgroundColor = "green"
+function enoughForClicker() {
+    if (counter >= clickerPrice) {
+        clicker.style.backgroundColor = "green";
     } else {
-        clicker.style.backgroundColor = "red"
+        clicker.style.backgroundColor = "red";
     }
 }
 
+if (clickerOwned >= 0) {
+    setInterval(() => {
+        counter += (clickerOwned * rebirth) * 0.2;
+        enoughForClicker();
+    }, 4000);
+}
 
+setInterval(() => {
+    counterElement.textContent = `Cookies: ${counter} / Rebirths: ${rebirth - 1} / Rebirth price: ${price}`;
+    clickerPriceElement.textContent = `Price: ${Math.round(clickerPrice)}`;
+    clickerOwnedElement.textContent = `Owned: ${clickerOwned}`;
+    enoughForClicker();
+}, 100);
